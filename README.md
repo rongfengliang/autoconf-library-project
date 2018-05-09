67addcce18df8c154b79d1722d4dfce8e3307d22
dalongrong <1141591465@qq.com>
2018-05-09 19:08:31 +0800

    add libcurl demo

---
+++ README.md
@@ -0,0 +1,11 @@
+# basic autoconf library demo
+
+use libtools
+
+## How to run
+
+```bash
+./autogen.sh
+./configure
+make
+```
---
+++ userservice.c
@@ -0,0 +1,12 @@
+#include "userservice.h"
+#include <stdio.h>
+
+// first function implement
+void first(){
+    printf("first function call");
+}
+
+// second function implement
+void second(){
+    printf("first function call");
+}
